05b7792d6564745ea338d179e72ca3825e372063
ultraplasm <alexey.katiev@gmail.com>
2024-10-11 12:31:51 +0300

    modal

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -5,6 +5,8 @@ import merch1 from '../Images/Merch/1.jpg';
 import merch2 from '../Images/Merch/2.PNG';
 import merch3 from '../Images/Merch/Тема 3.png';
 import merch4 from '../Images/Merch/4.png';
+import tvAnna from '../png/tvAnna.jpg'
+import untitled from '../png/untitled.jpg'
 
 const customStyles = {
     content: {
@@ -14,8 +16,12 @@ const customStyles = {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
-      width: '390px',
-      height: '30vh',
+      width: '60vw',
+      height: '80vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
       background: 'linear-gradient(127deg, rgba(255, 255, 255, 0.40) 10.63%, rgba(255, 255, 255, 0.1) 73.38%)',
       boxShadow: '0px 4px 24px -1px rgba(0, 0, 0, 0.25)',
       backdropFilter: 'blur(17.5px)',
@@ -29,6 +35,18 @@ const customStyles = {
 
 function Store() {
     const [isOpen, setIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [photo, setPhoto] = useState()
+    const [title, setTitle] = useState('')
+    const [text, setText] = useState('')
+
+    function openModal() {
+        setModalIsOpen(true);
+    }
+
+    function closeModal() {
+        setModalIsOpen(false);
+    }
 
     return (
         <div className="App2">
@@ -52,14 +70,14 @@ function Store() {
         }
         </div>
             <div class="content" style={{position: "absolute", top: "15vh", left: "19vw", width: "50vw" }}>
-                <div class="block" >
+                <div class="block" onClick={()=>{openModal(); setPhoto(tvAnna); setTitle('TV Anna'); setText('Видеоарт создан при помощи Феди Лакихила')}}>
                     <img src={merch3} style={{width: "20vw", height: "20vw"}}></img>
                     <div class="price"> 
                         <p>TV Anna</p>
                         <p>2023</p>
                     </div>
                 </div>
-                <div class="block" >
+                <div class="block" onClick={()=>{openModal(); setPhoto(untitled); setTitle('Untitled'); setText('В соавторстве с Александром Крыловым. Лучница, стреляя в пустоту, пытается противостоять неминуемой смерти всего живого. Цифровое бессмертие – форма материального существования – может отсрочить неотвратимую гибель, но никак не поможет избежать ее. Проект создан в рамках магистратуры «Технологическое искусство» в Университете МИСИС, 2023 год. Работа была представлена в Generative Gallery, в ЗИЛ на выставке Границы сред')}}>
                     <img src={merch1} style={{width: "20vw", height: "20vw"}}></img>
                     <div class="price"> 
                         <p>Untitled</p>
@@ -81,6 +99,16 @@ function Store() {
                     </div>
                 </div>
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+            >
+                <h2 class="modal-text" >{title}</h2>
+                <img src={photo} style={{width: "40vw", height: "20vw"}}></img>
+                <p class="modal-text" style={{ fontSize: '16px', marginTop: "18px", display: 'flex', flexDirection: 'column', alignItems: "center"}}>{text}</p>
+                <button class="continue" onClick={closeModal}>продолжить</button>
+            </Modal>
         </div>
     );
 }
